571f7ef0c09750d22695f7091625571222432699
tgendrich <gendricht@gmail.com>
2021-07-19 13:17:15 -0500

    initialization

---
+++ methods/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int A = 10;
+            int B = 10;
+            int C = 10;
+            int D = 10;
+            int[] array1 = { A, B, C, D };
+            Console.WriteLine("What is your name?");
+            String nameString = Console.ReadLine();
+            Console.WriteLine("What is the name of your pet?");
+            String petString = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            String colorString = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            String bandString = Console.ReadLine();
+            Console.WriteLine($"{nameString} is your name, {petString} is your pet, {colorString} is your favorite color, and {bandString} is your favorite band.");
+            Console.WriteLine($"{Program.Add(A, B)}");
+            Console.WriteLine($"{Program.Subtract(C, D)}");
+            var sum = Program.Addup(array1);
+            Console.WriteLine(sum);
+            
+        }
+        public static double Add (double A , double B)
+        {
+            return A + B;
+        }
+        public static double Subtract(double A, double B)
+        {
+            return A - B;
+        }
+        public static double Multiply(double A, double B)
+        {
+            return A * B;
+        }
+        public static double Quotient(double A, double B)
+        {
+            return A / B;
+
+        }
+        public static double Modulus(double A, double B)
+        {
+            return A % B; 
+        }
+        public static int Addup(params int[] values)
+        {
+            int sum = 0;
+            foreach (int i in values)
+            {
+                sum += i;
+
+            }
+            return sum;
+        }
+    }
+}
